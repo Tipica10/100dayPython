cb2deac5b519bbef785d83fb224f1a0db17afd7c
Theephiga-Balendran <Theephiga.Balendran@city.ac.uk>
2026-03-07 23:35:04 +0000

    Turtle Documentation and Tuples

---
+++ 18-/day18.py
@@ -0,0 +1,29 @@
+from turtle import Turtle, Screen
+import random
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("DarkSlateGray4")
+
+# for _ in range (10):
+#     timmy.forward(10)
+#     timmy.penup()
+#     timmy.forward(10)
+#     timmy.pendown()#
+
+#random walk
+# colors = ["navy", "olive", "DarkOrchid1", "coral" ,"blue", "beige", "DeepSkyBlue"]
+# directions = [0,90,180,270]
+# timmy.pensize(10)
+#
+# for i in range(200):
+#     timmy.forward(30)
+#     timmy.right(random.choice(directions))
+#     timmy.pencolor(random.choice(colors))
+#     timmy.speed(10)
+
+
+
+
+screen = Screen()
+screen.exitonclick()
