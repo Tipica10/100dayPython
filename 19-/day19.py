7c0035823ff4a5d3d557afa17f314035ed2787c6
Theephiga-Balendran <Theephiga.Balendran@city.ac.uk>
2026-03-09 13:37:40 +0000

    day19 project, created multiple turtle using one for loop and need to redo project later on

--- 19-/day19.py
+++ 19-/day19.py
@@ -1,29 +1,75 @@
+from platform import java_ver
 from turtle import Turtle, Screen
+import random
 
-tim = Turtle()
+is_race_on = False
 screen = Screen()
+screen.setup(500, 400)
+user_bet = screen.textinput(title= "Make your bet", prompt="Which turtle will win the race? Enter a colour: ")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_values = [-100,-50,-0,50,100,150]
+all_turtles = []
 
-def move_forwards():
-    tim.forward(10)
+for i in range(0,6):
+    new_turtle = Turtle(shape = "turtle")
+    new_turtle.penup()
+    new_turtle.color(colors[i])
+    new_turtle.goto(-230, y_values[i])
+    all_turtles.append(new_turtle)
 
-def move_backwards():
-    tim.backward(10)
-
-def counter_clock():
-    tim.left(10)
-
-def clockwise():
-    tim.right(10)
-
-def clear():
-    tim.reset()
+if user_bet:
+    is_race_on = True
 
 
-screen.listen()
-screen.onkey(key= "w", fun = move_forwards)
-screen.onkey(key= "s", fun = move_backwards)
-screen.onkey(key= "a", fun = counter_clock)
-screen.onkey(key= "d", fun = clockwise)
-screen.onkey(key= "c", fun = clear)
-#when applying function to another function lose the ()
+while is_race_on:
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"Wrong. The actual winner is {winning_color}.")
+        for_amount = random.randint(1, 10 + 1)
+        turtle.forward(for_amount)
+
+
+
+
+
+# tim = Turtle()
+# tim.penup()
+# tim.shape("turtle")
+# tim.color("green")
+# tim.goto(-230,-100)
+
+
+
+#
+# def move_forwards():
+#     tim.forward(10)
+#
+# def move_backwards():
+#     tim.backward(10)
+#
+# def counter_clock():
+#     tim.left(10)
+#
+# def clockwise():
+#     tim.right(10)
+#
+# def clear():
+#     tim.reset()
+#
+#
+# screen.listen()
+# screen.onkey(key= "w", fun = move_forwards)
+# screen.onkey(key= "s", fun = move_backwards)
+# screen.onkey(key= "a", fun = counter_clock)
+# screen.onkey(key= "d", fun = clockwise)
+# screen.onkey(key= "c", fun = clear)
+# #when applying function to another function lose the ()
+
+
+
 screen.exitonclick()
