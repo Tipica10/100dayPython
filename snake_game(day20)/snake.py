fb3f88b0bc46759d67c3917ea2e1c79ec455864a
Theephiga-Balendran <Theephiga.Balendran@city.ac.uk>
2026-03-12 12:00:04 +0000

    Snake_game - stuck on moving snake based on keys

---
+++ snake_game(day20)/snake.py
@@ -0,0 +1,49 @@
+from turtle import Turtle
+STARTING_POSITIONS = [(0,0), (-20,0), (-40,0)]
+MOVE_DISTANCE = 20
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
+
+    def right(self):
+        for seg_num in range(len(self.segments)-1, 0, -1):
+            new_xcor = self.segments[seg_num - 1].xcor()
+            new_ycor = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_xcor, new_ycor)
+        self.segments[0].forward(MOVE_DISTANCE)
+
+    def left(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_xcor = self.segments[seg_num - 1].xcor()
+            new_ycor = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_xcor, new_ycor)
+        self.segments[0].backward(MOVE_DISTANCE)
+
+    def up(self):
+        self.segments[0].left(90)
+        for seg_num in range(len(self.segments)-1, 0, -1):
+            new_xcor = self.segments[seg_num - 1].xcor()
+            new_ycor = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_xcor, new_ycor)
+        self.segments[0].forward(MOVE_DISTANCE)
+
+
+    def down(self):
+        self.segments[0].right(90)
+        for seg_num in range(len(self.segments)-1, 0, -1):
+            new_xcor = self.segments[seg_num - 1].xcor()
+            new_ycor = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_xcor, new_ycor)
+        self.segments[0].forward(MOVE_DISTANCE)
+
