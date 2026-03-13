76f4247ff0e8b7e9a3a82f6086c5c2e6a2abeab3
Theephiga-Balendran <Theephiga.Balendran@city.ac.uk>
2026-03-13 11:26:45 +0000

    Final SNAKE GAME

--- snake_game(day20)/main.py
+++ snake_game(day20)/main.py
@@ -8,7 +8,8 @@ screen = Screen()
 screen.setup(width=600,height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
-# screen.tracer(0)
+screen.tracer(0)
+# turning tracer off tells Turtle not he redraw the screen auto
 
 snake = Snake()
 food = Food()
@@ -20,14 +21,17 @@ screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
-
-screen.update()
 game_is_on = True
 
 while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+    # this creates a frame by frame animation
+    # move snake
+    # refresh screen
+    # wait
+    # repeat
 
     #detect collision with food
     if snake.head.distance(food) < 15:
