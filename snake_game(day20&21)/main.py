ce159c70d12a17dc9d3ce5d4415f24aa1a966d6e
Theephiga-Balendran <Theephiga.Balendran@city.ac.uk>
2026-03-16 12:05:47 +0000

    Snake game altered to incorporate High Score

--- snake_game(day20&21)/main.py
+++ snake_game(day20&21)/main.py
@@ -45,8 +45,8 @@ while game_is_on:
 
     #detect collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() >280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
     #detect collision with tail
     # for segment in snake.segments:
@@ -60,8 +60,9 @@ while game_is_on:
     #improve above code with slicing:
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
+
 
     #[1:] means everything but first segment which is head.
 
--- snake_game(day20&21)/snake.py
+++ snake_game(day20&21)/snake.py
@@ -24,6 +24,14 @@ class Snake:
         new_segment.goto(position)
         self.segments.append(new_segment)
 
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000,1000)
+        #this is because, the snake just dies there on screen, and we need it to move away so when we restart game, it is all new.
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
     def extend(self):
         self.add_segment(self.segments[-1].position())
 
